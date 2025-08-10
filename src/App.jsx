571f44603f33b432692e698eb9b741a235f9f4b5
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-08-10 16:07:55 +0300

    первая часть с jsonolaceholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
-import { createElement } from 'react';
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
+	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
+	useEffect(() => {
+		setIsLoading(true);
+
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos.slice(0, 15));
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Список дел</h1>
+			{isLoading ? (
+				<p>Загрузка...</p>
+			) : (
+				<ul className={styles.list}>
+					{todos.map((todo) => (
+						<li key={todo.id} className={styles.item}>
+							{todo.title}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
